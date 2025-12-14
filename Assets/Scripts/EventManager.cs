8db76cca92ca2eab7871fe0e3db0387c5f5d2529
MSI\nobuh <nobuhito110014@gmail.com>
2025-12-14 09:00:34 +0900

    色々統合

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,47 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
-using TMPro;
+using UnityEngine;
 
-public class TimerManager : SingletonBase<TimerManager>
+public class EventManager : SingletonBase<EventManager>
 {
+    [SerializeField] private int eventInterval;
+
     private readonly ReactiveProperty<int> _countdownTimePrep = new ReactiveProperty<int>(0);
-    public IReadOnlyReactiveProperty<int> CountdownTimePrep => _countdownTimePrep;
-    public float ElapsedTime => _countdownTimePrep.Value;
 
     private readonly CompositeDisposable _compositeDisposable = new CompositeDisposable();
 
     private float _accumulatedTime = 0f;
 
-    [SerializeField] private int timeLimit = 60;
-    [SerializeField] private int penaltySecond = 5;
-    public int TimeLimit { get; set; }
-
-    [SerializeField] private TextMeshProUGUI timerText;
-
-    public void Initialize()
-    {
-        _countdownTimePrep.Value = timeLimit;
-    }
-
-    [ContextMenu("Start Timer")]
-    public void StartTimer()
+    [ContextMenu("DebugStartEvent")]
+    public void StartEvent()
     {
         _compositeDisposable.Clear();
-        timerText.text = ToMin(_countdownTimePrep.Value);
 
         this.UpdateAsObservable()
             .Where(_ => _countdownTimePrep.Value > 0)
             .Subscribe(_ =>
             {
                 _accumulatedTime += Time.deltaTime;
-                if (_accumulatedTime >= 1f)
+                if (_accumulatedTime >= eventInterval)
                 {
                     _countdownTimePrep.Value -= (int)_accumulatedTime;
                     _accumulatedTime -= (int)_accumulatedTime;
-                    timerText.text = ToMin(_countdownTimePrep.Value);
+
+                    //randomƒCƒxƒ“ƒg
                 }
             })
             .AddTo(_compositeDisposable);
@@ -58,19 +46,10 @@ public class TimerManager : SingletonBase<TimerManager>
         _countdownTimePrep.Value = 0;
     }
 
-    public void PenaltyTime()
-    {
-        _countdownTimePrep.Value -= penaltySecond;
-    }
-
     protected override void OnDestroy()
     {
         _compositeDisposable.Dispose();
         base.OnDestroy();
     }
 
-    private string ToMin(int sec)
-    {
-        return ((sec / 60).ToString("D2") + ":" + (sec % 60).ToString("D2"));
-    }
 }
